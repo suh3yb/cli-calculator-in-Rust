db3a8e635e58af1915f30dcb92fd0b78c0209880
Suheyb Coskun <suheyb.c@gmail.com>
2023-01-30 14:18:34 +0100

    convert operator to char, use operate fn to get result

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ fn main() {
     let mut args: Args = args();
 
     let first: String = args.nth(1).unwrap();
-    let operator: String = args.nth(0).unwrap();
+    let operator: char = args.nth(0).unwrap().chars().next().unwrap();
     let second: String = args.nth(0).unwrap();
   
     let first_number = first.parse::<f32>().unwrap();
     let second_number = second.parse::<f32>().unwrap();
+    let result = operate(operator, first_number, second_number);
   
-    println!("{} {} {}", first_number, operator, second_number);
+    println!("{} {} {} {}", first_number, operator, second_number, result);
 }
 
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
