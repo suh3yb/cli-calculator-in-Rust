9f8ee1f33592b63bdcd913ec48a6500f561fd62c
Suheyb Coskun <suheyb.c@gmail.com>
2023-01-30 14:14:13 +0100

    add operate function

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,14 @@ fn main() {
   
     println!("{} {} {}", first_number, operator, second_number);
 }
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+    match operator {
+      '+' => first_number + second_number,
+      '-' => first_number - second_number,
+      '/' => first_number / second_number,
+      '*' | 'X' | 'x' => first_number * second_number,
+      _ => panic!("Invalid operator used."),
+    }
+  }
+  
