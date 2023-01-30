5b0cf776d5eeaf37002600a45a4da5d45a7c777e
Suheyb Coskun <suheyb.c@gmail.com>
2023-01-30 14:06:27 +0100

    get env arguments

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ fn main() {
     let mut args: Args = args();
     println!("{:?}", args);
 
-    let first: String = args.nth(1).unwrap();
-    println!("{}", first);
 
-    println!("args after using nth method on it: {:?}", args);
+  // The first argument is the location of the compiled binary, so skip it
+  let first: String = args.nth(1).unwrap();
+  // After accessing the second argument, the iterator's next element becomes the first
+  let operator: String = args.nth(0).unwrap();
+  let second: String = args.nth(0).unwrap();
+
+  println!("{} {} {}", first, operator, second);
 }
